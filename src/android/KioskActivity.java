536e771f6b9383363bba8ff1eb354da3f3475acc
Richard Lewin <richardlewin@Richards-MacBook-Pro.local>
2016-09-30 14:08:08 +0100

    Bug fixing

--- src/android/KioskActivity.java
+++ src/android/KioskActivity.java
@@ -180,7 +180,7 @@ public class KioskActivity extends CordovaActivity {
         View decorView = getWindow().getDecorView();
         int options;
         options =
-                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
                         //| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                         //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         //| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
