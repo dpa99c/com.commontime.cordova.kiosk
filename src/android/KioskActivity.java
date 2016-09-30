36d2ccb718819599229ee02fb7d64f727593ab1d
Richard Lewin <richardlewin@Richards-MacBook-Pro.local>
2016-09-30 14:04:10 +0100

    Bug fixing

--- src/android/KioskActivity.java
+++ src/android/KioskActivity.java
@@ -185,7 +185,7 @@ public class KioskActivity extends CordovaActivity {
                         //| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
                         //| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
                         //| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
-                        | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+                        //| View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
 
         decorView.setSystemUiVisibility(options);
         return decorView;
